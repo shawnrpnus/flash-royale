e5f7cbde3e6eff612bf60b502f9a1c13919cda51
E0175613 <E0175613@u.nus.edu>
2019-01-27 16:00:50 +0800

    Fix minor errors in sql statements

--- sql/createTable.sql
+++ sql/createTable.sql
@@ -11,7 +11,7 @@ manufacturer VARCHAR(128) NOT NULL,
 style VARCHAR(64) NOT NULL,
 color VARCHAR(64) NOT NULL,
 size VARCHAR(8) NOT NULL CHECK (size IN ('3XS', '2XS', 'XS', 'S', 'M', 'L', 'XL', '2XL', '3XL')),
-price NUMERIC NOT NULL
+price NUMERIC NOT NULL,
 image CHAR(16) UNIQUE
 );
 
@@ -19,7 +19,7 @@ CREATE TABLE stock (
 quantity NUMERIC NOT NULL,
 location_in_store VARCHAR(128) NOT NULL,
 store_name VARCHAR(128) NOT NULL,
-apparel_id NUMERIC NOT NULL,
+apparel_id INTEGER NOT NULL,
 PRIMARY KEY (store_name, apparel_id),
 FOREIGN KEY (store_name) references store(name),
 FOREIGN KEY (apparel_id) references apparel(id)
