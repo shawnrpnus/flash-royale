04006cb1dd7c010997b4b0a8e075a841e6d42ae8
mmdlow <mmdlow@gmail.com>
2019-01-29 17:50:45 +0800

    Update app.js css

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -51,37 +51,42 @@ class App extends Component {
 
   render() {
     return (
-      <div className='container text-center'>
-        {!(this.state.showCounter || this.state.showFittingRoom || this.state.showPhone || this.state.showFittingRoomSelection) ? 
-            <div>
-              <h1> Hello, select screen: </h1>
-              <div className="btn-group">
-                <button className="btn btn-primary" onClick={this.showCounter}>Counter</button>
-                <button className="btn btn-primary" onClick={this.showFittingRoomSelection}>Fitting Room</button>
-                <button className="btn btn-primary" onClick={this.showPhone}>Phone</button>
-              </div>
-    
-            </div> : <div></div>
-        }
-        {this.state.showCounter && <Counter/>}
-
-        {this.state.showFittingRoom ? <FittingRoom roomNumber={this.state.fittingRoomSelection}/>
-            :
-            this.state.showFittingRoomSelection ?
+      <div className="jumbotron jumbotron-fluid">
+        <div className='container text-center'>
+          {!(this.state.showCounter || this.state.showFittingRoom || this.state.showPhone || this.state.showFittingRoomSelection) ? 
               <div>
-                <select className="form-control" ref="dropdown">
-                  <option value="" disabled selected>Select fitting room number</option>
-                  <option value="1">1</option>
-                  <option value="2">2</option>
-                  <option value="3">3</option>
-                  <option value="4">4</option>
-                </select>
-                <button className="btn btn-primary" onClick={this.showFittingRoom}>Submit</button>
-              </div>
-              : <div></div>
-        }
+                <h1 className="display-4"> Greetings, human! </h1>
+                <p className="lead">Welcome to Smart Changing Room, the retail solution that changes your life.</p>
+                <hr></hr>
+                <p>Begin by selecting your screen below.</p>
+                <div className="btn-group">
+                  <button className="btn btn-primary" onClick={this.showCounter}>Counter</button>
+                  <button className="btn btn-primary" onClick={this.showFittingRoomSelection}>Fitting Room</button>
+                  <button className="btn btn-primary" onClick={this.showPhone}>Phone</button>
+                </div>
+      
+              </div> : <div></div>
+          }
+          {this.state.showCounter && <Counter/>}
 
-        {this.state.showPhone && <Phone/>}
+          {this.state.showFittingRoom ? <FittingRoom roomNumber={this.state.fittingRoomSelection}/>
+              :
+              this.state.showFittingRoomSelection ?
+                <div>
+                  <select className="form-control" ref="dropdown">
+                    <option value="" disabled selected>Select fitting room number</option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                  </select>
+                  <button className="btn btn-primary" onClick={this.showFittingRoom}>Submit</button>
+                </div>
+                : <div></div>
+          }
+
+          {this.state.showPhone && <Phone/>}
+        </div>
       </div>
     );
   }
